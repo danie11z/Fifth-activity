acf01d4b34e10456caf6a2bad2c6fe2ae41ff0ea
danie11z <daniel.estudando1@gmail.com>
2022-06-27 06:27:08 -0300

    5th activity

---
+++ script.js
@@ -0,0 +1,19 @@
+let nomes=['Tiago', 'Maria', 'Igor', 'Camila', 'Vitória']
+console.log(nomes)
+
+nomes.unshift('Lucas')
+console.log(nomes)
+
+nomes.pop()
+console.log(nomes)
+
+nomes.push('Rafaela', 'Gabriel')
+console.log(nomes)
+
+nomes.shift()
+console.log(nomes)
+
+let numbers=[7, 5, 6, 3, 8, 9, 2, 1, 4]
+
+numbers.sort()
+console.log(numbers)
